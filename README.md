99d8f8f1cec77189b71da76b93c986ebd660b2b8
Willy Clarke <willyclarke@mac.com>
2023-03-30 17:23:03 +0200

    BUGFIX: First working version

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # fluffy-palm-tree
-My adventures with Raylib
+
+## Description
+
+My adventures with Raylib.
+
+It continues on the initial FourierWithRaylib project that
+I created on [Github](https://github.com/willyclarke/fourierserieswithraylib.git).
+
+The code in here is motivated from an article written by Markus Buchholz, published on medium.
+
+His code can be found on [Github](https://github.com/markusbuchholz/Fourier-Series-in-Cpp-ImGui)
+
+His description can be found on [Medium](https://markus-x-buchholz.medium.com/fourier-series-in-c-imgui-1587e866586e)
+
+The cmake file is based on the example cmake file from the RayLib project.
+
+This provides a base project template which builds with [CMake](https://cmake.org).
+
+## Usage
+
+To compile the example, use one of the following dependending on your build target...
+
+### Desktop
+
+Use the following to build for desktop:
+
+``` bash
+rm -rf build
+cmake -B build
+cmake --build build
+```
+Or if Ninja build is preferred:
+
+```bash
+cmake -B build -G Ninja
+cmake --build build
+```
+
+## Dependencies
+
+* Gcc/G++
+* cmake
+* Raylib - gets pulled down with cmake's Fetchcontent.
+* On Linux some X-dev libraries are needed. CMake will complain and you need to install the missing libraries.
+
+** For Ubuntu 20.04 the following apt-packages are needed:
+```bash
+sudo apt install libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev
+```
+### Cmake on Ubuntu
+Cmake can be installed by both apt and snap on Ubuntu. Snap will install a newer version.
+```bash
+sudo snap install cmake --classic
+```
+
+## Runs on
+
+* OSX 13.2 (22D49) - Apple M2 Max
+* Ubuntu 20.04 - Parallels VM
+* Ubuntu 20.04 - On metal
+* Ubuntu 22.04 - Parallels VM
+
+### Web - untested
+
+Compiling for the web requires the [Emscripten SDK](https://emscripten.org/docs/getting_started/downloads.html):
+
+``` bash
+mkdir build
+cd build
+emcmake cmake .. -DPLATFORM=Web -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS="-s USE_GLFW=3" -DCMAKE_EXECUTABLE_SUFFIX=".html"
+emmake make
+```
---
+++ src/curvesrobotics.cpp
@@ -0,0 +1,401 @@
+
+/*******************************************************************************************
+ * Famous Curves for Robotics in C++
+ *
+ * Display of curves as suggested by Markus Buchholz on Medium.
+ * https://medium.com/@markus-x-buchholz/famous-curves-for-robotics-in-c-55aa916d1191
+ *
+ * Copyright (c) 2023 Willy Clarke
+ *
+ * MIT License
+ *
+ *******************************************************************************************/
+
+#include "engsupport.hpp"
+#include "raylib.h"
+
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace {
+/**
+ * Hold pixel position as integers, X and Y.
+ */
+struct pixel_pos {
+  int X{};
+  int Y{};
+};
+
+//------------------------------------------------------------------------------
+struct square_wave_elem {
+  float Amplitude{};
+  float Yn{};
+  float Xn{};
+  float Theta{};
+};
+
+//------------------------------------------------------------------------------
+struct data {
+  int screenWidth = 1280;
+  int screenHeight = 768;
+  int Key{};
+  int KeyPrv{};
+  bool StopUpdate{};
+  bool ShowGrid{true};
+  float Xcalc{};
+  float FofX{}; //!< Fourier calculated series value.
+  int n{5};     //!< Fourier series number of terms.
+  float dt{};
+  float t{};
+  std::vector<square_wave_elem> vSquareWaveElems{};
+  std::vector<Vector4> vTrendPoints{};
+  std::vector<pixel_pos> vGridLines{};
+
+  Matrix Hep{}; //!< Homogenous matrix for conversion from engineering space to
+                //!< pixelspace.
+
+  Vector4 vEngOffset{}; //!< Position of figure in engineering space.
+  Vector4 vPixelsPerUnit{100.f, 100.f, 100.f, 0.f};
+};
+
+/*
+ * Create lines and ticks for a grid in engineering units.
+ */
+auto vGridInPixels(Matrix const &Hep, //!< Homogenous matrix from engineering to
+                                      //!< pixel position.
+                   float GridXLowerLeft = -4.f, //!<
+                   float GridYLowerLeft = -3.f, //!<
+                   float GridLength = 8.f,      //!<
+                   float GridHeight = 6.f,      //!<
+                   float TickDistance = 0.1f    //!<
+                   ) -> std::vector<pixel_pos> {
+
+  // ---
+  // NOTE: Make and draw a grid.
+  // ---
+  struct grid_point {
+    float fromX{};
+    float fromY{};
+    float toX{};
+    float toY{};
+  };
+
+  const float NumTicksX = GridLength / TickDistance;
+  const float NumTicksY = GridHeight / TickDistance;
+  std::vector<grid_point> vGridPoint{};
+
+  //!< Vertical left
+  vGridPoint.push_back(grid_point{GridXLowerLeft, GridYLowerLeft,
+                                  GridXLowerLeft, GridYLowerLeft + GridHeight});
+
+  //!< Horizontal lower
+  vGridPoint.push_back(grid_point{GridXLowerLeft, GridYLowerLeft,
+                                  GridXLowerLeft + GridLength, GridYLowerLeft});
+
+  //!< Vertical rigth
+  vGridPoint.push_back(grid_point{GridXLowerLeft + GridLength, GridYLowerLeft,
+                                  GridXLowerLeft + GridLength,
+                                  GridYLowerLeft + GridHeight});
+
+  //!< Horizontal upper
+  vGridPoint.push_back(grid_point{GridXLowerLeft, GridYLowerLeft + GridHeight,
+                                  GridXLowerLeft + GridLength,
+                                  GridYLowerLeft + GridHeight});
+
+  //!< Center Horizontal
+  vGridPoint.push_back(grid_point{
+      GridXLowerLeft, GridYLowerLeft + (GridHeight / 2.f),
+      GridXLowerLeft + GridLength, GridYLowerLeft + (GridHeight / 2.f)});
+
+  //!< Center Vertical
+  vGridPoint.push_back(grid_point{
+      GridXLowerLeft + GridLength / 2.f, GridYLowerLeft,
+      GridXLowerLeft + GridLength / 2.f, GridYLowerLeft + GridHeight});
+
+  // ---
+  // NOTE: Create ticks along the horizontal axis.
+  // ---
+  for (size_t Idx = 0; Idx < int(NumTicksX); ++Idx) {
+    float const PosX0 = GridXLowerLeft + float(Idx) * TickDistance;
+    float const PosX1 = PosX0;
+    float const PosY0 = GridYLowerLeft + GridHeight / 2.f;
+    float const PosY1 = PosY0 + TickDistance / 2.f;
+    vGridPoint.push_back(grid_point{PosX0, PosY0, PosX1, PosY1});
+  }
+
+  // ---
+  // NOTE: Create ticks along the vertical axis.
+  // ---
+  for (size_t Idx = 0; Idx < int(NumTicksY); ++Idx) {
+    float const PosX0 = GridXLowerLeft + GridLength / 2.f;
+    float const PosX1 = PosX0 + TickDistance / 2.f;
+    float const PosY0 = GridYLowerLeft + float(Idx) * TickDistance;
+    float const PosY1 = PosY0;
+    vGridPoint.push_back(grid_point{PosX0, PosY0, PosX1, PosY1});
+  }
+
+  std::vector<pixel_pos> Result{};
+  for (auto Elem : vGridPoint) {
+    auto const ToPixel = Hep * es::Point(Elem.toX, Elem.toY, 0.f);
+    auto const FromPixel = Hep * es::Point(Elem.fromX, Elem.fromY, 0.f);
+    Result.push_back({int(ToPixel.x), int(ToPixel.y)});
+    Result.push_back({int(FromPixel.x), int(FromPixel.y)});
+  }
+
+  return Result;
+};
+
+/**
+ * Initialize the matrix to convert from engineering basis to screen basis.
+ * The screen center is used as the reference point.
+ *
+ * @OrigoScreen - the X, Y, Z values in engineering space at center of screen.
+ * @vPixelsPerUnit - The number of pixels per unit, i.e 100 pixels equals 1m.
+ * @ScreenCenterInPixels - The coordinates for the centre of the screen in
+ * pixels.
+ */
+auto InitEng2PixelMatrix(Vector4 const &OrigoScreen,
+                         Vector4 const &vPixelsPerUnit,
+                         Vector4 const &ScreenPosInPixels) -> Matrix {
+
+  // ---
+  // Flip because pixel coord increases when moving down.
+  // ---
+  constexpr float Flip = -1.f;
+  constexpr float NoFlip = 1.f;
+
+  // ---
+  // Create a Homogenous matrix that converts from engineering unit to screen.
+  // ---
+  auto Hes = es::I();
+  Hes.m12 = ScreenPosInPixels.x + OrigoScreen.x * vPixelsPerUnit.x;
+  Hes.m13 = ScreenPosInPixels.y + OrigoScreen.y * vPixelsPerUnit.y;
+  Hes.m14 = ScreenPosInPixels.z + OrigoScreen.z * vPixelsPerUnit.z;
+
+  // Flip and scale to pixel value.
+  Hes.m0 = NoFlip * vPixelsPerUnit.x;
+  Hes.m5 = Flip * vPixelsPerUnit.y;
+  Hes.m10 = NoFlip * vPixelsPerUnit.z;
+
+  return Hes;
+}
+
+/**
+ * Keyboard input handling common to all the drawing routines.
+ */
+auto HandleKeyboardInput(data *pData) -> bool {
+  bool InputChanged{};
+
+  constexpr float MinPixelPerUnit = 50.f;
+
+  if (pData->Key) {
+    if (KEY_G == pData->Key) {
+      pData->ShowGrid = !pData->ShowGrid;
+      InputChanged = true;
+    } else if (KEY_DOWN == pData->Key) {
+
+      auto &vPPU = pData->vPixelsPerUnit;
+      vPPU.x = std::max(vPPU.x - 10.f, MinPixelPerUnit);
+      vPPU.y = std::max(vPPU.y - 10.f, MinPixelPerUnit);
+      vPPU.z = std::max(vPPU.z - 10.f, MinPixelPerUnit);
+
+      InputChanged = true;
+    } else if (KEY_UP == pData->Key) {
+
+      auto &vPPU = pData->vPixelsPerUnit;
+      vPPU.x = std::max(vPPU.x + 10.f, MinPixelPerUnit);
+      vPPU.y = std::max(vPPU.y + 10.f, MinPixelPerUnit);
+      vPPU.z = std::max(vPPU.z + 10.f, MinPixelPerUnit);
+
+      InputChanged = true;
+    } else if (KEY_LEFT == pData->Key) {
+      --pData->n;
+      InputChanged = true;
+    } else if (KEY_RIGHT == pData->Key) {
+      ++pData->n;
+      InputChanged = true;
+    } else if (KEY_SPACE == pData->Key) {
+      InputChanged = true;
+      pData->StopUpdate = !pData->StopUpdate;
+    }
+
+    pData->KeyPrv = pData->Key;
+  }
+
+  if (InputChanged) {
+    pData->Xcalc = 0.0;
+    pData->vGridLines = vGridInPixels(pData->Hep);
+
+    pData->Hep = InitEng2PixelMatrix(
+        pData->vEngOffset, pData->vPixelsPerUnit,
+        {pData->screenWidth / 2.f, pData->screenHeight / 2.f, 0.f, 0.f});
+  }
+  return InputChanged;
+}
+
+/**
+ */
+auto UpdateDrawFrame(data *pData) -> void {
+  BeginDrawing();
+  ClearBackground(RAYWHITE);
+
+  DrawText(std::string("Use arrow keys. Zoom: " +
+                       std::to_string(pData->vPixelsPerUnit.x))
+               .c_str(),
+           140, 10, 20, BLUE);
+  DrawText(std::string("Num terms: " + std::to_string(pData->n) +
+                       ". Key:" + std::to_string(pData->KeyPrv) +
+                       ". Time:" + std::to_string(pData->Xcalc))
+               .c_str(),
+           140, 40, 20, BLUE);
+
+  bool const InputChanged = HandleKeyboardInput(pData);
+
+  if (pData->ShowGrid) {
+    for (size_t Idx = 0; Idx < pData->vGridLines.size(); Idx += 2) {
+      auto const &Elem0 = pData->vGridLines[Idx];
+      auto const &Elem1 = pData->vGridLines[Idx + 1];
+      DrawLine(Elem0.X, Elem0.Y, Elem1.X, Elem1.Y, Fade(VIOLET, 1.0f));
+    }
+  }
+
+  // ---
+  // NOTE: Lamda to draw a point.
+  // ---
+  auto ldaDrawPoint = [](Matrix const &Hep, Vector4 const &P,
+                         Vector4 const &m2Pixel, bool Print = false) -> void {
+    auto CurvePoint = Hep * P;
+    DrawPixel(CurvePoint.x, CurvePoint.y, RED);
+    constexpr float Radius = 0.01f;
+    DrawCircleLines(CurvePoint.x, CurvePoint.y, Radius * m2Pixel.x,
+                    Fade(BLUE, 0.3f));
+    if (Print) {
+      DrawLine(CurvePoint.x, CurvePoint.y, 0, 0, BLUE);
+      DrawText(std::string("CurvePoint x/y: " + std::to_string(CurvePoint.x) +
+                           " / " + std::to_string(CurvePoint.y))
+                   .c_str(),
+               140, 70, 20, BLUE);
+    }
+  };
+
+  auto ldaDrawCircle = [](Matrix const &Hep, Vector4 const &Centre,
+                          float Radius, Color Col = BLUE) -> void {
+    auto CurvePoint = Hep * Centre;
+    DrawCircleLines(CurvePoint.x, CurvePoint.y, Radius * Hep.m5,
+                    Fade(Col, 0.3f));
+  };
+
+  auto ldaDrawLine = [](Matrix const &Hep, Vector4 const &From,
+                        Vector4 const &To, Color Col = BLUE) -> void {
+    auto F = Hep * From;
+    auto T = Hep * To;
+    DrawLine(F.x, F.y, T.x, T.y, BLUE);
+  };
+
+  auto const Frequency = 2.0;
+  auto const Omegat = M_2_PI * Frequency * pData->t;
+  auto const Radius = 4.f / M_PI;
+  auto Centre = es::Point(-5.f, 0.f, 0.f);
+
+  auto Ft =
+      Centre + es::Vector(Radius * cosf(Omegat), Radius * sinf(Omegat), 0.f);
+
+  ldaDrawCircle(pData->Hep, Centre, Radius);
+
+  // Draw the outer circle line
+  ldaDrawLine(pData->Hep, Centre, Ft);
+
+  // ---
+  // Create the Fourier series.
+  // ---
+  auto Ftp = Ft;
+  for (int Idx = 1; Idx < pData->n; ++Idx) {
+    auto nthTerm = 1.f + Idx * 2.f;
+    auto Ftn = Ftp + es::Vector(Radius / nthTerm * cosf(nthTerm * Omegat),
+                                Radius / nthTerm * sinf(nthTerm * Omegat), 0.f);
+    ldaDrawLine(pData->Hep, Ftp, Ftn);
+    ldaDrawCircle(pData->Hep, Ftn, Radius / nthTerm);
+    Ftp = Ftn;
+  }
+
+  auto GridStart = es::Point(0.f, 0.f, 0.f);
+  pData->Xcalc += pData->dt;
+
+  // Reset time - based on knowing that the default grid settings are from -4 to
+  // +4. FIXME.
+  if (pData->Xcalc > 4.f) {
+    pData->Xcalc = -4.f;
+    pData->vTrendPoints.clear();
+  }
+
+  auto AnimationPoint = GridStart + es::Vector(pData->Xcalc, Ftp.y, 0.f);
+
+  // Draw the actual trend
+  pData->vTrendPoints.push_back(AnimationPoint);
+  for (auto E : pData->vTrendPoints) {
+    ldaDrawPoint(pData->Hep, E, {pData->Hep.m0, pData->Hep.m5, 0.f, 0.f});
+  }
+
+  // Draw the inner circle line
+  ldaDrawLine(pData->Hep, Ft, Ftp);
+  // Draw the connecting line
+  ldaDrawLine(pData->Hep, Ftp, AnimationPoint);
+
+  EndDrawing();
+}
+}; // namespace
+
+/**
+ *
+ */
+auto main() -> int {
+  data Data{};
+  Data.vTrendPoints.reserve(size_t(Data.screenWidth));
+  auto pData = &Data;
+
+  // ---
+  // Initialization
+  // ---
+  InitWindow(Data.screenWidth, Data.screenHeight,
+             "Fourier terms on a square wave");
+
+  SetTargetFPS(60); // Set our game to run at X frames-per-second
+
+  // ---
+  // NOTE: Move all points around by setting the engineering offset which will
+  // be added to the offset of the screen position in pixels.
+  // ---
+  Data.vEngOffset = es::Point(0.f, 0.f, 0.f);
+  Data.vPixelsPerUnit = es::Point(100.f, 100.f, 0.f);
+
+  // ---
+  // NOTE: Set up Homogenous matrix for conversion to pixel space.
+  // ---
+  Data.Hep = InitEng2PixelMatrix(
+      Data.vEngOffset, Data.vPixelsPerUnit,
+      {Data.screenWidth / 2.f, Data.screenHeight / 2.f, 0.f, 0.f});
+
+  Data.vGridLines = vGridInPixels(Data.Hep);
+
+  // Main game loop
+  while (!WindowShouldClose()) // Detect window close button or ESC key
+  {
+    DrawFPS(10, 10);
+    Data.dt = 1. / 60.f; // / GetFPS();
+    if (!Data.StopUpdate)
+      Data.t = GetTime();
+    Data.Key = GetKeyPressed();
+
+    UpdateDrawFrame(pData);
+  }
+
+  // ---
+  // De-Initialization
+  // ---
+  CloseWindow(); // Close window and OpenGL context
+
+  return 0;
+}
