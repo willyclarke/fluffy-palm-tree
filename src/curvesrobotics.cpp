529b810c0e879f49ce4d1f11bc14a7dbaf18f8a9
Willy Clarke <willyclarke@mac.com>
2023-04-02 12:16:06 +0200

    BUGFIX: Add page firstscan and WikipediaLink

--- src/curvesrobotics.cpp
+++ src/curvesrobotics.cpp
@@ -16,6 +16,7 @@
 
 #include <algorithm>
 #include <cmath>
+#include <cstdio>
 #include <iomanip>
 #include <iostream>
 #include <string>
@@ -28,6 +29,9 @@ namespace {
 struct pixel_pos {
   int X{};
   int Y{};
+  Color color{LIGHTGRAY};
+  std::string TxtTagX{}; //!< Text for x markers
+  std::string TxtTagY{}; //!< Text for y markers
 };
 
 /**
@@ -38,6 +42,7 @@ struct pixel_pos {
  */
 struct grid_cfg {
   std::vector<pixel_pos> vGridLines{};
+  std::vector<pixel_pos> vGridSubDivider{};
 
   /**
    */
@@ -56,9 +61,11 @@ struct data {
   // Declare the function pointer
   auto(*UpdateDrawFramePointer)(data *) -> void;
   std::vector<std::string> vHelpTextPage{};
+  std::string WikipediaLink{};
 
   int screenWidth = 1280;
   int screenHeight = 768;
+  int PageNum{};
   int Key{};
   int KeyPrv{};
   bool StopUpdate{};
@@ -101,11 +108,14 @@ auto GridCfgInPixels(Matrix const &Hep, //!< Homogenous matrix from engineering
     float fromY{};
     float toX{};
     float toY{};
+    bool TagX{};
+    bool TagY{};
   };
 
   const float NumTicksX = GridLength / TickDistance;
   const float NumTicksY = GridHeight / TickDistance;
   std::vector<grid_point> vGridPoint{};
+  std::vector<grid_point> vGridSubDivider{};
 
   //!< Vertical left
   vGridPoint.push_back(grid_point{GridXLowerLeft, GridYLowerLeft,
@@ -139,11 +149,27 @@ auto GridCfgInPixels(Matrix const &Hep, //!< Homogenous matrix from engineering
   // NOTE: Create ticks along the horizontal axis.
   // ---
   for (size_t Idx = 0; Idx < int(NumTicksX); ++Idx) {
-    float const PosX0 = GridXLowerLeft + float(Idx) * TickDistance;
-    float const PosX1 = PosX0;
-    float const PosY0 = GridYLowerLeft + GridHeight / 2.f;
-    float const PosY1 = PosY0 + TickDistance / 2.f;
-    vGridPoint.push_back(grid_point{PosX0, PosY0, PosX1, PosY1});
+    auto const PosX0 = GridXLowerLeft + float(Idx) * TickDistance;
+    auto const PosX1 = PosX0;
+    auto const PosY0 = GridYLowerLeft + GridHeight / 2.f;
+    auto const PosY1 = PosY0 + TickDistance / 2.f;
+
+    // ---
+    // NOTE: Tag at each major divider to ease text placement.
+    // ---
+    auto const TagX = (Idx && !(Idx % 5));
+    auto const TagY{false};
+    vGridPoint.push_back(grid_point{PosX0, PosY0, PosX1, PosY1, TagX, TagY});
+
+    // ---
+    // NOTE: Create the vGridSubDivider horizontally.
+    // ---
+    if (Idx && !(Idx % 5)) {
+      auto const PosSudividerY0 = GridYLowerLeft;
+      auto const PosSudividerY1 = GridYLowerLeft + GridHeight;
+      vGridSubDivider.push_back(
+          grid_point{PosX0, PosSudividerY0, PosX1, PosSudividerY1});
+    }
   }
 
   // ---
@@ -155,6 +181,23 @@ auto GridCfgInPixels(Matrix const &Hep, //!< Homogenous matrix from engineering
     float const PosY0 = GridYLowerLeft + float(Idx) * TickDistance;
     float const PosY1 = PosY0;
     vGridPoint.push_back(grid_point{PosX0, PosY0, PosX1, PosY1});
+
+    // ---
+    // NOTE: Tag at each major divider to ease text placement.
+    // ---
+    auto const TagX{false};
+    auto const TagY = (Idx && !(Idx % 5));
+    vGridPoint.push_back(grid_point{PosX0, PosY0, PosX1, PosY1, TagX, TagY});
+
+    // ---
+    // NOTE: Create the vGridSubDivider vertically.
+    // ---
+    if (Idx && !(Idx % 5)) {
+      auto const PosSudividerX0 = GridXLowerLeft;
+      auto const PosSudividerX1 = GridXLowerLeft + GridLength;
+      vGridSubDivider.push_back(
+          grid_point{PosSudividerX0, PosY0, PosSudividerX1, PosY1});
+    }
   }
 
   grid_cfg Result{};
@@ -163,7 +206,48 @@ auto GridCfgInPixels(Matrix const &Hep, //!< Homogenous matrix from engineering
   Result.GridDimensions.x = GridLength;
   Result.GridDimensions.y = GridHeight;
 
-  for (auto Elem : vGridPoint) {
+  // ---
+  // NOTE: Create major dividers.
+  // ---
+  for (auto const &Elem : vGridPoint) {
+    auto const ToPixel = Hep * es::Point(Elem.toX, Elem.toY, 0.f);
+    auto const FromPixel = Hep * es::Point(Elem.fromX, Elem.fromY, 0.f);
+
+    // ---
+    // NOTE: Convert the floating point indicators.
+    // ---
+    auto ldaFloat2Str = [](float In) -> std::string {
+      std::string Result{};
+      struct converted_text {
+        char Conv[10]{};
+      };
+
+      converted_text C{};
+
+      auto const Status =
+          std::snprintf(C.Conv, sizeof(converted_text), "%.1f", In);
+
+      if (Status) {
+        Result = std::string(C.Conv);
+      }
+      return Result;
+    };
+
+    // ---
+    // NOTE: Create the axis tag based on the setup from the grid.
+    // ---
+    auto const &TagX = Elem.TagX ? ldaFloat2Str(Elem.fromX) : "";
+    auto const &TagY = Elem.TagY ? ldaFloat2Str(Elem.fromY) : "";
+    pixel_pos PixelPos = {int(ToPixel.x), int(ToPixel.y), DARKGRAY, TagX, TagY};
+
+    Result.vGridLines.push_back(PixelPos);
+    Result.vGridLines.push_back({int(FromPixel.x), int(FromPixel.y), DARKGRAY});
+  }
+
+  // ---
+  // NOTE: Create the minor dividers.
+  // ---
+  for (auto const &Elem : vGridSubDivider) {
     auto const ToPixel = Hep * es::Point(Elem.toX, Elem.toY, 0.f);
     auto const FromPixel = Hep * es::Point(Elem.fromX, Elem.fromY, 0.f);
     Result.vGridLines.push_back({int(ToPixel.x), int(ToPixel.y)});
@@ -235,7 +319,7 @@ auto ldaDrawCircle = [](Matrix const &Hep, Vector4 const &Centre, float Radius,
                         Color Col = BLUE) -> void {
   auto CurvePoint = Hep * Centre;
   // Use Hep.m5 for scaling/zoom factor.
-  DrawCircleLines(CurvePoint.x, CurvePoint.y, Radius * Hep.m5, Fade(Col, 0.3f));
+  DrawCircleLines(CurvePoint.x, CurvePoint.y, Radius * Hep.m5, Fade(Col, 0.9f));
 };
 
 /**
@@ -266,7 +350,14 @@ auto ldaShowGrid = [](data *pData) -> void {
   for (size_t Idx = 0; Idx < pData->GridCfg.vGridLines.size(); Idx += 2) {
     auto const &Elem0 = pData->GridCfg.vGridLines[Idx];
     auto const &Elem1 = pData->GridCfg.vGridLines[Idx + 1];
-    DrawLine(Elem0.X, Elem0.Y, Elem1.X, Elem1.Y, Fade(VIOLET, 1.0f));
+
+    DrawLine(Elem0.X, Elem0.Y, Elem1.X, Elem1.Y, Fade(Elem0.color, 0.3f));
+
+    if (!Elem0.TxtTagX.empty())
+      DrawText(Elem0.TxtTagX.c_str(), Elem0.X - 1, Elem0.Y + 8, 10, DARKGRAY);
+
+    if (!Elem0.TxtTagY.empty())
+      DrawText(Elem0.TxtTagY.c_str(), Elem0.X - 20, Elem0.Y - 10, 10, DARKGRAY);
   }
 };
 
@@ -326,9 +417,14 @@ auto HandleKeyboardInput(data *pData) -> bool {
     } else if (KEY_F == pData->Key) {
       pData->UpdateDrawFramePointer = &UpdateDrawFrameFourier;
       InputChanged = true;
+    } else if (KEY_L == pData->Key) {
+      if (!pData->WikipediaLink.empty())
+        OpenURL(pData->WikipediaLink.c_str());
     } else if (KEY_F1 == pData->Key) {
       pData->UpdateDrawFramePointer = &UpdateDrawFrameHelp;
       InputChanged = true;
+    } else if (KEY_F2 == pData->Key) {
+      TakeScreenshot("./Fluffy.png");
     }
 
     pData->KeyPrv = pData->Key;
@@ -439,13 +535,20 @@ auto UpdateDrawFrameFourier(data *pData) -> void {
 }
 
 /**
- * Draw an animation of n - terms of a Fourier square wave.
+ * Draw an animation of the Asteroid parametric equation.
+ * link: https://en.wikipedia.org/wiki/Astroid
  */
 auto UpdateDrawFrameAsteroid(data *pData) -> void {
+
+  if (2 != pData->PageNum) {
+    pData->WikipediaLink = "https://en.wikipedia.org/wiki/Astroid";
+    pData->PageNum = 2;
+  }
+
   BeginDrawing();
   ClearBackground(WHITE);
 
-  DrawText(std::string(". Key:" + std::to_string(pData->KeyPrv) +
+  DrawText(std::string("Asteriode. Key:" + std::to_string(pData->KeyPrv) +
                        ". Time:" + std::to_string(pData->Xcalc))
                .c_str(),
            140, 40, 20, BLUE);
@@ -547,6 +650,11 @@ auto UpdateDrawFrameAsteroid(data *pData) -> void {
  * Display a page with some help text.
  */
 auto UpdateDrawFrameHelp(data *pData) -> void {
+
+  if (0 != pData->PageNum) {
+    pData->PageNum = 0;
+  }
+
   BeginDrawing();
   ClearBackground(LIGHTGRAY);
 
@@ -579,6 +687,9 @@ auto UpdateDrawFrameHelp(data *pData) -> void {
  */
 auto main(int argc, char const *argv[]) -> int {
 
+  // ---
+  // NOTE: Poor mans testing framework.
+  // ---
   if (argc > 1) {
     es::TestHomogenousMatrix();
     es::Test3dCalucations();
@@ -600,8 +711,10 @@ auto main(int argc, char const *argv[]) -> int {
              "Fluffy's adventures with Raylib");
 
   Data.vHelpTextPage.push_back("F1 - This help page");
-  Data.vHelpTextPage.push_back("f -  Fourier square wave");
+  Data.vHelpTextPage.push_back("F2 - ScreenShot");
   Data.vHelpTextPage.push_back("a -  Asteriode");
+  Data.vHelpTextPage.push_back("f -  Fourier square wave");
+  Data.vHelpTextPage.push_back("l -  Open current page's web link");
 
   // ---
   SetTargetFPS(60); // Set our game to run at X frames-per-second
@@ -625,7 +738,7 @@ auto main(int argc, char const *argv[]) -> int {
   // ---
   Data.GridCfg = GridCfgInPixels(Data.Hep);
 
-  Data.UpdateDrawFramePointer = UpdateDrawFrameFourier;
+  Data.UpdateDrawFramePointer = UpdateDrawFrameHelp;
 
   // ---
   // Main game loop
